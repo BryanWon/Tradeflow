80e22260285352f814c9226980ed4067a92c3cf1
BryanWon <2303069@sit.singaporetech.edu.sg>
2024-06-11 15:07:11 +0800

    img change

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -1,5 +1,5 @@
 
-<link rel="shortcut icon" type="image/x-icon"  href="../img/favicon.png">
+<link rel="shortcut icon" type="image/x-icon"  href="../img/favicon.jpg">
 <meta charset="utf-8">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
